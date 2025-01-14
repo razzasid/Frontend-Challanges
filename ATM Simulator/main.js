93317027beea73dee5187e3a587c09753b1941d8
Mohd Raza <razamohd6395@gmail.com>
2025-01-14 22:13:43 +0530

    update ATM simulator

--- ATM Simulator/main.js
+++ ATM Simulator/main.js
@@ -6,34 +6,70 @@ const checkBalace = document.getElementById("checkBalance");
 const deposit = document.getElementById("deposit");
 const withdraw = document.getElementById("withdraw");
 let balance = 1000;
+let isLogOut = false;
 
 function logIn(pin) {
-  //   console.log(newpin);
+  //   console.log(pin);
   if (pin === "1234") {
     output.textContent = "Login successful.";
     logOut.disabled = false;
+    isLogOut = true;
   } else {
     output.textContent = "Invalid PIN. Please try again.";
+    isLogOut = false;
   }
 }
 
 submitBtn.addEventListener("click", () => {
-  const pin = input.value;
+  let pin = input.value;
   logIn(pin);
+  input.value = "";
+  //   console.log(logIn());
+});
+
+logOut.addEventListener("click", () => {
+  isLogOut = false;
+  logOut.disabled = true;
+  output.textContent = "Logged out successfully.";
 });
 
 checkBalace.addEventListener("click", () => {
-  output.textContent = "Your balance is $1000";
+  if (isLogOut) {
+    output.textContent = `Your balance is ${balance}`;
+  } else {
+    output.textContent = `Please login first.`;
+  }
 });
 
 deposit.addEventListener("click", () => {
-  let depositAmnt = parseInt(prompt("Enter the amount to deposit:"));
-  console.log(depositAmnt);
+  if (isLogOut) {
+    let depositAmnt = parseInt(prompt("Enter the amount to deposit:"));
+    console.log(depositAmnt);
 
-  if (isNaN(depositAmnt)) {
-    output.textContent = "Invalid amount. Please try again.";
+    if (isNaN(depositAmnt)) {
+      output.textContent = "Invalid amount. Please try again.";
+    } else {
+      balance += depositAmnt;
+      output.textContent = `Deposited ${depositAmnt}. Your new balance is ${balance}`;
+    }
   } else {
-    balance += depositAmnt;
-    output.textContent = `Deposited ${depositAmnt}. Your new balance is ${balance}`;
+    output.textContent = `Please login first.`;
+  }
+});
+
+withdraw.addEventListener("click", () => {
+  if (isLogOut) {
+    let withdrawAmnt = parseInt(prompt("Enter the amount to withdraw:"));
+    console.log(withdrawAmnt);
+
+    if (isNaN(withdrawAmnt) || withdrawAmnt > balance) {
+      output.textContent =
+        "Invalid amount or insufficient balance. Please try again.";
+    } else {
+      balance -= withdrawAmnt;
+      output.textContent = `Withdrawn ${withdrawAmnt}. Your new balance is ${balance}`;
+    }
+  } else {
+    output.textContent = `Please login first.`;
   }
 });
